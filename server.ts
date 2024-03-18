52a6afa267464ceb9753c57616da5f5e263d6738
Andrew Robie <arobie1992@yahoo.com>
2024-03-18 19:46:09 -0400

    Some cleanup

--- server.ts
+++ server.ts
@@ -1,6 +1,3 @@
-import ffmpeg from 'npm:fluent-ffmpeg';
-ffmpeg.setFfmpegPath('/Users/Andrew/node_modules/@ffmpeg-installer/darwin-x64/ffmpeg');
-
 const port= 8080;
 
 const handler = (request: Request): Promise<Response>|Response => {
@@ -28,49 +25,10 @@ const videoHandler = (request: Request): Response => {
     const { socket, response } = Deno.upgradeWebSocket(request);
     socket.onopen = async () => {
         console.log("beginning sending video");
-        // ffmpegStream(socket);
         await fileStream(socket);
     }
     socket.onclose = () => console.log('closed with remaining:', socket.bufferedAmount);
     return response;
-}
-
-const ffmpegStream = (socket: WebSocket) => {
-    let finished = false;
-
-    let i = 0;
-    const start = performance.now();
-    const command = ffmpeg("./video/fragged-SampleVideo_1280x720_30mb.mp4")
-        // finally! https://stackoverflow.com/questions/49429954/mfcreatefmpeg4mediasink-does-not-generate-mse-compatible-mp4
-        .outputOptions(['-movflags frag_keyframe+empty_moov+default_base_moof'])
-        .format('mp4')
-        .audioCodec('copy')
-        .videoCodec('copy')
-        .on('error', (err: string) => console.log('An error occurred: ' + err))
-        .on('end', () => {
-            finished = true;
-        });
-
-    const q: any[] = [];
-    const ffstream = command.pipe();
-    ffstream.on('data', (chunk: any) => {
-        i += chunk.length;
-        q.push(chunk);
-    });
-
-    const throttle = () => {
-        if(finished && !q.length) {
-            socket.close(1000, "stream finished");
-            console.log('Processing finished:', i, "bytes", performance.now() - start, "ms");
-            return;
-        }
-        const chunk = q.shift();
-        if(chunk) {
-            socket.send(chunk);
-        }
-        setTimeout(throttle, 10);
-    }
-    throttle();
 }
 
 const fileStream = async (socket: WebSocket) => {
@@ -89,7 +47,6 @@ const throttledSend = async (send: (buff: Uint8Array) => void , cleanup: () => v
     const data = body.value;
     send(data);
     totalSent += data.length;
-    // console.log(totalSent);
     setTimeout(() => throttledSend(send, cleanup, f, totalSent), 1);
 }
 
